92dd8af40c97775262feb9c08ff7216a284df163
H3xept <h3xept@gmail.com>
2019-02-24 00:59:24 +0000

    Improved reading mechanism

--- src/readline.c
+++ src/readline.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include "line_tools.h"
 #include <ctype.h>
+#include <stdarg.h>
 
 // -- Begin Termios Config --
 static struct termios* termios_data = NULL;
@@ -29,9 +30,13 @@ void reset_termios_data() {
 // -- End Termios Config --
 
 unsigned int handle_control_key(char c, char* current_buffer, unsigned int* cursor_location) {
-	unsigned int ret = 1;
+	int ret = 0;
 	switch(c) {
-		
+
+		case 0x0a:
+			printf("\n");
+			ret = -1;
+			break;
 		/*
 			I detect some trickery!
 			Mac OS (♡) handles delete and backspace in the same way - aka only backspace is implemented.
@@ -53,45 +58,49 @@ unsigned int handle_control_key(char c, char* current_buffer, unsigned int* curs
 	
 		case 0x03:
 			printf("\n");
-			exit(-1);
-		default:
-			ret = 0;
+			ret = -1;
+			break;
 	}
 	return ret;
 }
+
 char* read_line(char* prompt) {
 	
 	#define MAX_BUFFER_SIZE 500
 	
-	char current_buffer[MAX_BUFFER_SIZE] = {0};
+	char* current_buffer = calloc(MAX_BUFFER_SIZE, sizeof(char));
+
 	unsigned int cursor_location = 0;
 	
 	printf("%s",prompt);
-	
+
 	struct termios new_termios_data = copy_current_termios_data();
 	new_termios_data.c_iflag &= ~(IXON);
 	new_termios_data.c_lflag &= ~(ICANON|ECHO|ISIG|IEXTEN);
+	new_termios_data.c_cc[VMIN] = 0;
+	new_termios_data.c_cc[VTIME] = 1;
+
 	store_termios_data(new_termios_data);
 	
 	atexit(reset_termios_data);	
-	
+	setvbuf(stdout, (char *)NULL, _IONBF, 0); 
+
 	while(1) {
-		char c = getchar();
-//		if (iscntrl(c)) {
-//			printf("\nControl! %d",c);
-//		}
-		if (!(handle_control_key(c, current_buffer, &cursor_location))) {
-			
-			assert(cursor_location >= 0);
-			
+		char c = '\0';
+		if (read(STDIN_FILENO, &c, 1) == -1) exit(33);
+		if (iscntrl(c)) {
+			if (handle_control_key(c, current_buffer, &cursor_location) == -1) {
+				return current_buffer;
+			}
+		} else {
 			if (!(cursor_location+1 > MAX_BUFFER_SIZE)) {
-				printf("%c",c);
+				printf("%c", c);
+				fflush(stdout);
 				current_buffer[cursor_location++] = c;	
 			}
-			
 		}
 	}
 	
 	reset_termios_data();
-	return "";
+	return current_buffer;
 }
