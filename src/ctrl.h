97232398bfb7d75ea5c324090423f53bcc63050a
H3xept <h3xept@gmail.com>
2019-02-25 22:50:56 +0000

    Custom CTRL_

--- src/ctrl.h
+++ src/ctrl.h
@@ -2,7 +2,7 @@
 #define __CLTR__
 
 #define CTRL_(C) (0x1f&(C))
-#define ASCII_CONTROL_C CTRL('c')
+#define ASCII_CONTROL_C CTRL_('c')
 #define ASCII_CONTROL 0x1b
 #define ASCII_BACKSPACE 0x08
 #define ASCII_DELETE 0x7f
